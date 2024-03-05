50aec18592bf63c5050066992e54df124a49da4f
v1ct0r16 <pvictor1844@gmail.com>
2024-03-05 14:09:03 +0100

    commit messgae

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,17 +8,20 @@ import { productEntity } from 'src/Entity/product.entity';
         TypeOrmModule.forRootAsync({
           useFactory: (configService: ConfigService) => ({
             type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: '',
-            database: 'crud',
+            host: configService.getOrThrow('DB_HOST'),
+            port: configService.getOrThrow('DB_PORT'),
+            username: configService.getOrThrow('DB_USER'),
+            password: configService.getOrThrow('DB_PASSWORD'),
+            database: configService.getOrThrow('DB_DATABASE'),
             entities: [productEntity],
-            synchronize: true,
+            synchronize: configService.getOrThrow('DB_SYNC'),
           }),
-          
          inject: [ConfigService]
         }),
+
+        
       ]
+
+    
 })
 export class DatabaseModule {}
